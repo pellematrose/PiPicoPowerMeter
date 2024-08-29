82439aa0b1a31a66dd66ce9823301df1693ddafd
pellematrose <75641459+pellematrose@users.noreply.github.com>
2024-08-29 09:07:48 +0200

    Update main.py
    Added two more buttons, different sample times for oled, measurement and csv, code cleanup

--- PPPM_SW/main.py
+++ PPPM_SW/main.py
@@ -12,9 +12,10 @@
 # consistent with other displays.
 # V0.32 5th Nov 2020 Replace uos.urandom for minimal ports
 
-# Copyright (c) 2024 Per-Simon Saal
+# Last edit: 29.08.2024
+# Copyright (c) 2023 Per-Simon Saal
 
-from machine import Pin, I2C
+from machine import Pin, I2C, UART
 from struct import unpack
 from time import sleep
 import time
@@ -22,9 +23,12 @@ import math
 from drivers.ina219 import ina219
 
 import utime
+            
 from color_setup import ssd
+                                                                
 from gui.core.writer import Writer
 from gui.core.nanogui import refresh
+                                   
 from gui.widgets.label import Label
 
 # Fonts
@@ -37,7 +41,6 @@ import gui.fonts.freesans20 as free20
 
 #INA219 CONFIG
 INA_ADDR = 64
-SHUNT_OHMS = 0.1 #R
 
 VBUS_RANGE_16 = 0x00	#BRNG
 VBUS_RANGE_32 = 0x01
@@ -79,23 +82,49 @@ MODE_SHUNT_C     = 0x05
 MODE_VOLT_C      = 0x06
 MODE_SHUNT_BUS_C = 0x07	#Shunt and bus, continuous
 
+# actual used mode (edit here)
+SHUNT_OHMS = 0.1 #R
+VBUS = VBUS_RANGE_32
+VSHUNT = VSHUNT_MAX_4
+BADC = BADC_12BIT
+SADC = SADC_12BIT
+MODE = MODE_SHUNT_BUS_C
+
 mWh = 0
-actual_time = 0
-oled_last_time = 0
+
+# helper variables to run the code periodically
+actual_time_oled = 0 
+actual_time_meas = 0 
+actual_time_csv = 0
+last_time_oled = 0
+last_time_meas = 0
+last_time_csv = 0
 toggle_time = 0
+
+# refresh rate of take and show measurements in ms. EDIT here:
+refresh_rate_oled = 250
+refresh_rate_meas = 250
+refresh_rate_csv = 500
 
 # page of the UI
 page = 0
-# refresh rate of take and show measurements in ms
-oled_refresh_rate = 250
 
 # initialize i2c (which i2cx, GPNr., GPNr., frequency Hz)
 i2c_ina  = I2C(1, scl = Pin(15), sda = Pin(14), freq = 400000)
+
+# initialize UART on port 1 pins 4 & 5
+uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5), bits=8, parity=None, stop=1)
 
 # initialize button
 button = Pin(9, Pin.IN, Pin.PULL_UP)
 btn_debounce_t = 0
 btn_isr_flag = 0
+
+button2 = Pin(8, Pin.IN, Pin.PULL_UP)
+btn_isr_flag2 = 0
+
+button3 = Pin(1, Pin.IN, Pin.PULL_UP)
+btn_isr_flag3 = 0
 
 # for logging
 file_name = "Logging.csv"
@@ -108,9 +137,9 @@ led2.value(0)
 
 # Create current measuring object
 ina = ina219(INA_ADDR, i2c_ina)
-ina.configure(SHUNT_OHMS, VBUS_RANGE_16, VSHUNT_MAX_2, BADC_12BIT, SADC_12BIT, MODE_SHUNT_BUS_C)
+ina.configure(SHUNT_OHMS, VBUS, VSHUNT, BADC, SADC, MODE)
     
-# Write initial Text on OLED
+# Write initial Text on OLED                
 ssd.text("Power Meter", 0, 0)
 ssd.show()
 
@@ -123,7 +152,28 @@ def btn_callback(button):
         btn_debounce_t=time.ticks_ms()
         
 button.irq(trigger=button.IRQ_FALLING, handler=btn_callback)
+   
+def btn2_callback(button2):
+    global btn_isr_flag2, btn_debounce_t
+    if (time.ticks_ms()-btn_debounce_t) > 200:
+        btn_isr_flag2= 1
+        btn_debounce_t=time.ticks_ms()
         
+button2.irq(trigger=button.IRQ_FALLING, handler=btn2_callback)
+
+def btn3_callback(button3):
+    global btn_isr_flag3, btn_debounce_t
+    if (time.ticks_ms()-btn_debounce_t) > 200:
+        if btn_isr_flag3 == 0:
+            btn_isr_flag3 = 1
+        else:
+            btn_isr_flag3 = 0           
+        #btn_isr_flag3 = 1
+        btn_debounce_t=time.ticks_ms()
+        led2.toggle()
+        
+button3.irq(trigger=button.IRQ_FALLING, handler=btn3_callback)
+   
 def format_number(num):
     if num >= 100:
         return f"{int(num)}"
@@ -134,7 +184,7 @@ def format_number(num):
 
 def show_value(label, wert):
 
-    if (wert < 10 and wert >= 1):
+    if (wert < 40 and wert >= 1):
         d = "x"
         if label == "Voltage":
             d = "V"
@@ -168,31 +218,31 @@ def show_value(label, wert):
     wri2.set_clip(False, False, False)
     Label(wri2, 27, 0, '            ', bdcolor=False)
     Label(wri2, 27, 0, w, bdcolor=False)
-    
+
     refresh(ssd)
     
 def multi_show_values(voltage, current, power):
     u = f"{voltage:.3f}{"V"}"
     i = f"{current:.3f}{"A"}"
     p = f"{power:.3f}{"W"}"
-    
+                                                                     
     wri = Writer(ssd, free20, verbose=False)
     wri.set_clip(False, False, False)
     Label(wri, 2, 2, 'Overview', bdcolor=False)
-    
+
     Writer.set_textpos(ssd, 20, 0)  # In case previous tests have altered it
     wri = Writer(ssd, small, verbose=False)
     wri.set_clip(False, False, False)
 
-    Label(wri, 23, 2, 'U:', bdcolor=False)
+    Label(wri, 23, 2, 'U:', bdcolor=False)                                                               
     Label(wri, 23, 15, '                    ', bdcolor=False) # draw black space to erase former value
     Label(wri, 23, 15, u, bdcolor=False)
 
-    Label(wri, 36, 2, 'I:', bdcolor=False)
+    Label(wri, 36, 2, 'I:', bdcolor=False)                                                                  
     Label(wri, 36, 15, '                    ', bdcolor=False) # draw black space to erase former value
     Label(wri, 36, 15, i, bdcolor=False)
     
-    Label(wri, 49, 2, 'P:', bdcolor=False)
+    Label(wri, 49, 2, 'P:', bdcolor=False)                                                                  
     Label(wri, 49, 15, '                    ', bdcolor=False) # draw black space to erase former value
     Label(wri, 49, 15, p, bdcolor=False)    
     
@@ -200,12 +250,13 @@ def multi_show_values(voltage, current, power):
     
 while True:
     # get system tick
-    actual_time = time.ticks_ms()
+    actual_time_meas = time.ticks_ms()
+    actual_time_oled = time.ticks_ms()
+    actual_time_csv = time.ticks_ms()
         
-    # take and show measurements 'oled_refresh_rate' seconds
-    if actual_time - oled_last_time > oled_refresh_rate:
-
-        oled_last_time = actual_time
+    # take and show measurements 'refresh_rate' seconds
+    if actual_time_meas - last_time_meas > refresh_rate_meas:
+        last_time_meas = actual_time_meas
     
         v = ina.vbus()
         #c = ina.vshunt()
@@ -217,28 +268,38 @@ while True:
         #print("v = %.6fV" % v ,", i = %.6fA" % i, " P = %.6fW" % p, " E = %.6fmWh" % mWh)
         #print("%.3f"%v,",%.3f"%i,",%.2f"%p,"\n")
         
-        if page == 0:
+        # Output for Serial Studio
+        #print('/*' + str(v) + ',' + str(i) + ',' + str(p) + '*/\n')
+     
+    if actual_time_oled - last_time_oled > refresh_rate_oled:
+        last_time_oled = actual_time_oled
+         
+        if page == 0:               
             show_value("Voltage", v)
-        elif page == 1:
+        elif page == 1:             
             show_value("Current", i)
-        elif page == 2:
+        elif page == 2:      
             show_value("Power", p)
-#         elif page == 3:
+#         elif page == 3:    
 #             show_value("Engergy", mWh)
-        elif page == 3:
+        elif page == 3:             
             multi_show_values(v, i, p)
         
-        # logging u, i and p to a csv
-#         with open (file_name,"a") as logging:
-#             logging.write(f'{str(v)}')
-#             logging.write(",")
-#             logging.write(f'{str(i)}')
-#             logging.write(",")
-#             logging.write(f'{str(p)}{"\n"}')
-#             logging.flush()
-#         logging.close()
-
-    
+    if actual_time_csv - last_time_csv > refresh_rate_csv:
+        last_time_csv = actual_time_csv                                                      
+         
+        if btn_isr_flag3 is 1:
+            btn_isr_flag3 = 0                      
+            #logging u, i and p to a csv
+            with open (file_name,"a") as logging:
+                logging.write(f'{str(v)}')
+                logging.write(",")
+                logging.write(f'{str(i)}')
+                logging.write(",")
+                logging.write(f'{str(p)}{"\n"}')
+                logging.flush()
+            logging.close()
+                                  
     # todo when button is pressed
     if btn_isr_flag is 1:
         btn_isr_flag = 0
@@ -246,18 +307,20 @@ while True:
         if page < 3:
             page += 1
             ssd.fill(0)	# clear display when open new page
-            #print("Page: %d" %page)
-
         elif page == 3:
             page = 0
-            ssd.fill(0)
-            #print("Page: %d" %page)
-            
-        # add custom button code here
+            ssd.fill(0)            
     
+    if btn_isr_flag2 is 1:
+        btn_isr_flag2 = 0
+        if page > 0:
+            page -= 1
+            ssd.fill(0)
+        elif page == 0:
+            page = 3
+            ssd.fill(0)                     
+
     # just toggle the led1
-    if actual_time - toggle_time > 1000:
+    if actual_time_oled - toggle_time > 1000:
         toggle_time = time.ticks_ms()
         led1.toggle()
-    
-    
